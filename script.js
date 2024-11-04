0a1925b7830856ccc54c3f66b92a2e38a22f8f3d
juancruz922 <113599381+juancruz922@users.noreply.github.com>
2024-11-03 21:01:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
 // JavaScript para el acordeón
 document.querySelectorAll('.accordion-button').forEach(button => {
-    button.addEventListener('click', () => {
-        // Alternar la clase "active" para el botón
-        button.classList.toggle('active');
-
-        // Alternar la visibilidad del contenido
-        const content = button.nextElementSibling;
-        content.classList.toggle('open'); // Alternar la clase "open"
-        
-        // Cambiar la rotación de la flecha
-        const arrow = button.querySelector('.accordion-arrow');
-        if (content.classList.contains('open')) {
-            arrow.style.transform = 'rotate(180deg)'; // Flecha hacia la derecha
-        } else {
-            arrow.style.transform = 'rotate(360deg)'; // Flecha hacia abajo
-        }
-    });
+    button.addEventListener('click', toggleAccordion);
+    button.addEventListener('touchstart', toggleAccordion); // Agregar este evento
 });
+
+function toggleAccordion() {
+    // Alternar la clase "active" para el botón
+    this.classList.toggle('active');
+
+    // Alternar la visibilidad del contenido
+    const content = this.nextElementSibling;
+    content.classList.toggle('open'); // Alternar la clase "open"
+    
+    // Cambiar la rotación de la flecha
+    const arrow = this.querySelector('.accordion-arrow');
+    if (content.classList.contains('open')) {
+        arrow.style.transform = 'rotate(180deg)'; // Flecha hacia arriba
+    } else {
+        arrow.style.transform = 'rotate(0deg)'; // Flecha hacia abajo
+    }
+}
 
 
