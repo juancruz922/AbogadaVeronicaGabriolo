8982cc6da41c909eb951dbc441dc63ec8579cf04
juancruz922 <113599381+juancruz922@users.noreply.github.com>
2024-11-03 21:19:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ document.querySelectorAll('.accordion-button').forEach(button => {
 });
 
 function toggleAccordion() {
-    this.classList.toggle('active');
     const content = this.nextElementSibling;
-    content.classList.toggle('open');
     
-    const arrow = this.querySelector('.accordion-arrow');
-    if (content.classList.contains('open')) {
-        arrow.style.transform = 'rotate(180deg)'; // Flecha hacia arriba
-    } else {
-        arrow.style.transform = 'rotate(0deg)'; // Flecha hacia abajo
-    }
+    // Usar un temporizador para manejar el evento
+    setTimeout(() => {
+        this.classList.toggle('active');
+        content.classList.toggle('open');
+        
+        const arrow = this.querySelector('.accordion-arrow');
+        if (content.classList.contains('open')) {
+            arrow.style.transform = 'rotate(180deg)'; // Flecha hacia arriba
+        } else {
+            arrow.style.transform = 'rotate(0deg)'; // Flecha hacia abajo
+        }
+    }, 100); // Retraso de 100 ms
 }
